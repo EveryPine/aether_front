5c9b3396c7271e2ce68ae34a7d8d3459649ca7e1
jyy0913 <chanel.gjf@gmail.com>
2025-02-06 15:19:56 +0900

    refactor: 코멘트 리스트 동적 렌더링 수정

--- src/components/TaskComment/CommentList.tsx
+++ src/components/TaskComment/CommentList.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Profile from '../../assets/Profile-small.svg'
 
 const CommentList: React.FC = () => {
-  return (
-    <div className="absolute left-[128px] top-[230px] w-[464px] px-5 py-4 bg-[#f3f5f8] rounded-lg border border-[#e5eaf2] flex-col justify-start items-start gap-1 inline-flex">
-    <div className="justify-start items-center gap-3 inline-flex">
-        <div className="justify-start items-center gap-2 flex">
-        <div className="w-3 h-3 justify-start items-center gap-2.5 flex">
-            <img
-              src={Profile}
-              className="grow shrink basis-0 self-stretch rounded-full" 
-            />
+  const [commets, setComments] = useState([
+    {
+      id: 1,
+      name: "최기수",
+      timeAgo: "3시간 전",
+      content: "ABCD하고 ABCD 하도록 합시다.",
+    },
+    {
+      id: 2,
+      name: "최기수",
+      timeAgo: "3시간 전",
+      content: "ABCD하고 ABCD 하도록 합시다.",
+    },
+  ])
+
+  return(
+    <div className="absolute w-[464px] h-auto overflow-y-auto max-h-[500px] left-[128px] top-[230px] flex flex-col gap-5">
+      {commets.map((comment) => (
+        <div key={comment.id} className="self-stretch px-5 py-4 bg-[#f3f5f8] rounded-lg border border-[#e5eaf2] flex-col justify-start items-start gap-1 inline-flex">
+        <div className="justify-start items-center gap-3 inline-flex">
+          <div className="justify-start items-center gap-2 flex">
+            <div className="w-3 h-3 justify-start items-center gap-2.5 flex">
+              <img
+                src={Profile}
+                className="grow shrink basis-0 self-stretch rounded-full" 
+              />
+            </div>
+            <div className="text-[#4f5462] text-xs font-medium leading-none">{comment.name}</div>
+          </div> 
+          <div className="justify-start items-center gap-2 flex">
+            <div className="text-[#949bad] text-xs font-medium leading-none">{comment.timeAgo}</div>
+          </div>
         </div>
-        <div className="text-[#4f5462] text-xs font-medium leading-none">최기수</div>
-        </div> 
-        <div className="justify-start items-center gap-2 flex">
-        <div className="text-[#949bad] text-xs font-medium leading-none">3시간 전</div>
-        </div>
-    </div>
-    <div className="text-[#4f5462] text-sm font-medium leading-relaxed">ABCD하고 ABCD 하도록 합시다.</div>
+        <div className="text-[#4f5462] text-sm font-medium leading-relaxed">{comment.content}</div>
+      </div>
+      ))}
     </div>
   );
 };
