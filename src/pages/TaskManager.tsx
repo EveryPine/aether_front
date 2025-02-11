07090154acdf5bdca49083b78bece0f5e9dced43
jyy0913 <chanel.gjf@gmail.com>
2025-02-11 22:28:21 +0900

    refactor: taskManager를 업무 생성에서 재사용 가능하도록 수정

--- src/pages/TaskManager.tsx
+++ src/pages/TaskManager.tsx
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import TaskTitle from '../components/TaskTitle'
 import Sidebar from '../components/TaskSidebar'
 import Header from '../components/TaskHeader'
 import TaskDivider from '../components/TaskDivider'
 import Manager from '../components/TaskManager/Manager'
 
-const TaskManager = () => {
+interface TaskManagerProps {
+  mode: 'add' | 'setting';  // 'add'는 업무 생성, 'setting'은 업무 설정
+}
+
+const TaskManager: React.FC<TaskManagerProps> = ({ mode }) => {
+  const [title, setTitle] = useState(mode === 'add' ? '' : 'ABC 업무');
   return (
     <div className="w-[640px] h-[972px] relative bg-[#F8F9FC] rounded-tl-lg shadow-[inset_0px_0px_8px_0px_rgba(26,26,35,0.12)]  overflow-hidden">
       <Header />
       <div>
-        <Sidebar />
+        <Sidebar visibleItems = {mode === 'add' ? ['info', 'user'] : ['info', 'chat', 'docu', 'user']}/>
         <main>  
           {/* 업무 이름 */}
-          <TaskTitle />
+          <TaskTitle isEditable= {mode === 'add'} title={title} setTitle={mode === 'add' ? setTitle : undefined}/>
           <h4 className="absolute h-[28px] left-[128px] top-[174px] text-[#4f5462] text-xl font-semibold leading-7">
             담당자
           </h4>
-          <TaskDivider />
+          <TaskDivider top={mode === 'add' ? '152px' : '136px'} />
           <Manager />
         </main>
       </div>
